fe6f1366cccd7776e75658662648b1a3a456431b
leesanghun-droid <53643793+leesanghun-droid@users.noreply.github.com>
2024-02-08 09:14:18 +0900

    .
    .

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart3
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart3.Legends.Add(legend1);
+            this.chart3.Location = new System.Drawing.Point(0, 0);
+            this.chart3.Name = "chart3";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart3.Series.Add(series1);
+            this.chart3.Size = new System.Drawing.Size(1500, 750);
+            this.chart3.TabIndex = 0;
+            this.chart3.Text = "chart3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1592, 849);
+            this.Controls.Add(this.chart3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Series Chart1;
+        Series Chart2;
+        Series Chart3;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            Console.WriteLine("start~");
+            chart3.Series.Clear();
+
+            Chart1 = chart3.Series.Add("POS");
+            Chart2 = chart3.Series.Add("VEL");
+            Chart3 = chart3.Series.Add("ACC");
+
+            Title title = new Title();
+            title.Text = "pos";
+            title.ForeColor = Color.Blue;
+            title.Font = new Font("맑은고딕", 10, FontStyle.Bold);
+            chart3.Titles.Add(title);
+
+            Chart1.LegendText = "POS";
+            Chart1.ChartType = SeriesChartType.Line;
+            Chart1.Color = Color.Red;
+            Chart1.BorderWidth = 1;
+
+            Chart2.LegendText = "VEL";
+            Chart2.ChartType = SeriesChartType.Line;
+            Chart2.Color = Color.Green;
+            Chart2.BorderWidth = 1;
+
+            Chart3.LegendText = "ACC";
+            Chart3.ChartType = SeriesChartType.Line;
+            Chart3.Color = Color.Yellow;
+            Chart3.BorderWidth = 1;
+
+            chart3.ChartAreas[0].AxisY.Minimum = -7000;
+            chart3.ChartAreas[0].AxisY.Maximum = +7000;
+
+            chart3.ChartAreas[0].AxisY.LabelStyle.Interval = 1000;
+            chart3.ChartAreas[0].AxisY.MajorGrid.Interval = 1000;
+
+
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 10;
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+
+        }
+
+        double delta_t = 0.001;
+        double now_time = 0;
+        double time_t = 0;
+        double vel = 0;
+        double ex_vel = 0;
+        double pos = 0;
+        double ex_pos = -0.0001;
+        double acc = 0;
+
+        public void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            int total = Chart1.Points.Count();
+
+            var pi = Math.PI;
+            //double acc = Math.Sin(now_time*2*pi);
+            //vel = vel + acc * delta_t;
+
+            pos = 1.5 * Math.Sin(now_time * 2 * pi * 7);
+            vel = (pos - ex_pos) * (1 / delta_t);
+            acc = (vel - ex_vel) * (1 / delta_t);
+            ex_pos = pos;
+            ex_vel = vel;
+
+
+            if (total < (1/ delta_t))
+            {
+                chart3.Invoke(new MethodInvoker(delegate
+                {
+
+                    Chart1.Points.AddXY(now_time, pos);
+                    Chart2.Points.AddXY(now_time, vel);
+                    Chart3.Points.AddXY(now_time, acc);
+                    //Console.WriteLine(total);
+                }));
+                
+            }
+
+            now_time += delta_t;
+        }
+    }
+}
